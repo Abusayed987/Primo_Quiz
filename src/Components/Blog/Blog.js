4cbcf7430dae1bf0653cea034ee91479d4137871
Md.Abu Sayed <sayedshishir.abu@gmail.com>
2022-11-30 18:05:29 +0600

    added Blog section Question and ans

--- src/Components/Blog/Blog.js
+++ src/Components/Blog/Blog.js
@@ -3,7 +3,26 @@ import './Blog.css'
 const Blog = () => {
     return (
         <div className='blog'>
-            <h1>Blog</h1>
+            <div>
+                <h2>Question: 01 <br /> <br />
+                    What's the purpose of React  Router ?
+                </h2>
+                <p>Ans: The React Router is mainly used for developing Single Page Web Applications. React Router is used to define multiple routes in the application. The main purpose  of library for routing in React. It enables the navigation among views of various components in a React Application, allows changing the browser URL, and keeps the UI in sync with the URL. </p>
+            </div>
+            <hr />
+            <div>
+                <h2>Question: 02 <br /> <br />
+                    How Does Context API works ?
+                </h2>
+                <p>Ans:The Context API is the alternative to "prop drilling" or moving props from grandparent to child to parent, and so on. Context API passing data from a parent to a deeply nested child. Context API provides a scalable and simple framework, making it a great way to implement shared data between components And Read that value within any component by using the context consumer. </p>
+            </div>
+            <hr />
+            <div>
+                <h2>Question: 03 <br /> <br />
+                    What is useRef Hook in React Router?
+                </h2>
+                <p>Ans: The React Router is mainly used for developing Single Page Web Applications. React Router is used to define multiple routes in the application. The main purpose  of library for routing in React. It enables the navigation among views of various components in a React Application, allows changing the browser URL, and keeps the UI in sync with the URL. </p>
+            </div>
         </div>
     );
 };
